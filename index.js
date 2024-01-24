970f27cff4eeda02619668e6c9d7f51d56f0a9e3
Tyler Ralston <TRalston511@gmail.com>
2024-01-24 09:52:57 -0500

    inquirer version update and questions added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const inquirer = required("inquirer");
 
 // Array of Questions to Utilize Input and Generate README 
 
-const questions = {
+const questions = [
     {
         type: "input",
         name: "title",
@@ -16,4 +16,24 @@ const questions = {
         name: "user story",
         message: "Describe the User Story for this Project"
     },
-}
+    {
+        type: "input",
+        name: "resources",
+        message: "List any Languages and Technologies used"
+    },
+    {
+        type: "input",
+        name: "repo link",
+        message: "Share your link to repository"
+    },
+    {
+        type: "input",
+        name: "app link",
+        message: "Share your link to application"
+    },
+    {
+        type: "input",
+        name: "github link",
+        message: "Share your link to your github"
+    },
+]
