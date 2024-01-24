cd9630bda8d6312416a22823bdb7c693ea335819
Tyler Ralston <TRalston511@gmail.com>
2024-01-24 09:22:07 -0500

    adding questions

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 // Packages needed for the application
+
 const fs = required("fs");
 const inquirer = required("inquirer");
-const path = required("path");
+
+// Array of Questions to Utilize Input and Generate README 
+
+const questions = {
+    {
+        type: "input",
+        name: "title",
+        message: "Name the Project"
+    },
+    {
+        type: "input",
+        name: "user story",
+        message: "Describe the User Story for this Project"
+    },
+}
